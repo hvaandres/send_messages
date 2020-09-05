f4c37096125a5fa7d2a5fca2e06f665b56028b06
Andres Haro <hvaandres@icloud.com>
2020-09-05 14:03:50 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import config
+
+"""
+We understand that Sapmming people is wrong but if you would like to do this for fun, you will have to test it with your own telephone number.
+At this point, I'm not responsible for the bad usage of this script!
+
+"""
+
+def get_words(file_path):
+    with open(file_path, 'r') as f:
+        text = f.readlines()[0]
+        words = text.split()
+    return words
+
+def get_lines(file_path):
+    with open(file_path, 'r') as f:
+        text = f.readlines()
+    return text
+
+def send_message(phone_number, message):
+    os.system('osascript send.scpt {} "{}"'.format(phone_number, message))
+
+if __name__ == '__main__':
+    #words = get_words('ly.txt')
+    #for word in words:
+        #send_message('6197516857', word)
+
+
+    text = get_lines('ly.txt')
+    for line in text:
+        send_message('insert_phone_number', line)
